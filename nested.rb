54a0cdfec6c01a5ec9e89206af10e000ab062479
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-02 06:36:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,34 +14,16 @@ def alan_kay_is_known_for
 
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
         :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+          :known_for => "Object Orientation"
+					}
         }
-     }
 end
 
 def dennis_ritchies_language
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
         :dennis_ritchie => {
-          :known_for => "Unix",
           :languages => ["C"]
         }
      }
@@ -56,21 +38,11 @@ def adding_matz
 
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+        :yukihiru_matsumoto => {
+					:known_for => "Ruby",
+					:languages => ["LISP", "C"]
+				}
      }
-
-
 end
 
 def changing_alan
@@ -91,8 +63,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
@@ -111,5 +82,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages].include "Assembly"
 end
