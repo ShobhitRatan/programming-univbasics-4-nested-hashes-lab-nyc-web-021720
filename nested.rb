623ece5bd1b1e1e44908c9bf56b6e477637ad40c
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-02 06:54:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,5 +82,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].include "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] include "Assembly"
 end
